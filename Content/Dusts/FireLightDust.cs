ffa7fae2be56a810b586b05c603d65db7af9239e
Monomon <3152865050@qq.com>
2025-03-01 17:00:14 +0800

    纳米粒子炮重做,邪炎焚除制作完成

---
+++ Content/Dusts/FireLightDust.cs
@@ -0,0 +1,41 @@
+using GloryofGuardian.Common;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GloryofGuardian.Content.Dusts
+{
+    public class FireLightDust : ModDust
+    {
+        public override string Texture => GOGConstant.Dusts + "Dust1";
+
+        public override void OnSpawn(Dust dust) {
+            dust.fadeIn = 0;//可以用作计时器
+            dust.scale *= Main.rand.NextFloat(0.8f, 1.5f);
+        }
+
+        public override bool Update(Dust dust) {
+            dust.fadeIn++;
+            dust.color *= 0.01f;
+            if (dust.fadeIn > 5) {
+                dust.active = false;
+            }
+            dust.position += dust.velocity;
+            //dust.rotation = dust.velocity.ToRotation() + MathHelper.PiOver2;
+            return false;
+        }
+
+        public override bool PreDraw(Dust dust) {
+            Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
+            Color drawColor = new Color(255, 50, 50, 120);
+
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Additive, SamplerState.AnisotropicWrap, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.ZoomMatrix);
+
+            Main.EntitySpriteDraw(texture, dust.position - Main.screenPosition, null, drawColor, 0, texture.Size() / 2, 0.08f * dust.scale, 0, 0f);
+
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, Main.GameViewMatrix.TransformationMatrix);
+
+            return false;
+        }
+    }
+}
---
+++ Content/Projectiles/MaliceFireGunFireProj.cs
@@ -0,0 +1,78 @@
+﻿using GloryofGuardian.Common;
+using GloryofGuardian.Content.Dusts;
+using System;
+using Terraria.DataStructures;
+using Terraria.ID;
+
+namespace GloryofGuardian.Content.Projectiles
+{
+    public class MaliceFireGunFireProj : GOGProj
+    {
+        public override string Texture => GOGConstant.Asset + "placeholder";
+
+        public override void SetStaticDefaults() {
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 20;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+        }
+
+        public override void SetDefaults() {
+            Projectile.width = 16;
+            Projectile.height = 16;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.DamageType = DamageClass.Summon;
+            Projectile.timeLeft = 60;
+            Projectile.light = 3.0f;
+            Projectile.ignoreWater = false;
+            //Projectile.usesLocalNPCImmunity = true;
+            //Projectile.localNPCHitCooldown = 60;
+            Projectile.idStaticNPCHitCooldown = 12;
+            Projectile.aiStyle = -1;
+            Projectile.penetrate = -1;
+
+            Projectile.extraUpdates += 3;
+        }
+
+        Player Owner => Main.player[Projectile.owner];
+
+        public override void OnSpawn(IEntitySource source) {
+            count += Main.rand.Next(4);
+        }
+
+        int count = 0;
+        public override void AI() {
+            count++;
+
+            float dustscale = count / 8;
+            float dustvel = count / 8f;
+
+            if (Projectile.wet) Projectile.Kill();
+
+            //粒子
+            int dustnum = Math.Min((int)(count / 30f) + 3, 6);
+
+            if (count % 4 == 0) {
+                int num1 = Dust.NewDust(Projectile.position, Projectile.width / 2, Projectile.height / 2, ModContent.DustType<FireLightDust>(), 0, 0, 0, default, 0.5f);
+                Main.dust[num1].noGravity = true;
+                Main.dust[num1].velocity = new Vector2(0, -dustvel);
+                Main.dust[num1].scale *= (1 + dustscale);
+            }
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
+            Projectile.velocity *= 0.8f;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity) {
+
+            return base.OnTileCollide(oldVelocity);
+        }
+
+        public override Color? GetAlpha(Color lightColor) {
+            return Color.White;
+        }
+
+        public override void OnKill(int timeLeft) {
+        }
+    }
+}
