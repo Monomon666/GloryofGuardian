ac34f50ec120a5663ae6a06ec27f5e37fb343787
Monomon <3152865050@qq.com>
2025-03-02 12:29:28 +0800

    肉后版本的血腥和腐化炮台完成初版制作

---
+++ Content/Projectiles/Corrupt2DT.cs
@@ -0,0 +1,199 @@
+﻿using GloryofGuardian.Common;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.DataStructures;
+using Terraria.ID;
+
+namespace GloryofGuardian.Content.Projectiles
+{
+    public class Corrupt2DT : GOGDT
+    {
+        //不使用贴图,重写绘制
+        public override string Texture => GOGConstant.nulls;
+        public override void SetStaticDefaults() {
+        }
+
+        public sealed override void SetDefaults() {
+            Projectile.width = 28;
+            Projectile.height = 32;
+            Projectile.tileCollide = true;
+
+            Projectile.friendly = true;
+            Projectile.DamageType = GuardianDamageClass.Instance;
+            Projectile.penetrate = -1;
+            Projectile.scale *= 1f;
+            Projectile.timeLeft = 36000;
+
+            Projectile.scale *= 1f;
+        }
+
+        Player Owner => Main.player[Projectile.owner];
+
+        //防止破坏地图道具
+        public override bool? CanCutTiles() {
+            return false;
+        }
+
+        //生成时自由下坠
+        public override void OnSpawn(IEntitySource source) {
+            count0 = 120;//默认发射间隔
+            Projectile.velocity = new Vector2(0, 8);
+            base.OnSpawn(source);
+        }
+
+        int count = 0;
+        int count0 = 0;
+        //重力
+        bool drop = true;
+        //数据读取
+        int Gcount = 0;
+        int lastdamage = 0;
+        public override void AI() {
+            count++;
+            Projectile.timeLeft = 2;
+            Projectile.StickToTiles(false, false);//形成判定
+            Drop();
+            Calculate();
+            //索敌与行动
+            NPC target1 = Projectile.Center.InPosClosestNPC(800, true, true);
+            if (target1 != null) {
+                Attack(target1);
+            }
+
+            base.AI();
+        }
+
+        /// <summary>
+        /// 坠落
+        /// </summary>
+        void Drop() {
+            Projectile.velocity.Y += 0.2f;
+            if (Projectile.velocity.Y > 8f) {
+                Projectile.velocity.Y = 8f;
+            }
+
+            Vector2 droppos = Projectile.Bottom;
+            if (drop) {
+                int maxdropdis = 5000;
+                for (int y = 0; y < maxdropdis; y++) {
+                    Tile tile0 = TileHelper.GetTile(GOGUtils.WEPosToTilePos(droppos + new Vector2(0, y) * 16));
+                    if (tile0.HasTile) {
+                        Projectile.Bottom = (droppos + new Vector2(0, y - 6) * 16);
+                        break;
+                    }
+                }
+                drop = false;
+            }
+        }
+
+        /// <summary>
+        /// 重新计算和赋值参数
+        /// </summary>
+        void Calculate() {
+            Gcount = (int)(count0 * Owner.GetModPlayer<GOGModPlayer>().GcountR * Projectile.ai[0]);//攻击间隔因子重新提取
+            //伤害修正
+            int newDamage = Projectile.originalDamage;
+            float rangedOffset = Owner.GetTotalDamage(GuardianDamageClass.Instance).ApplyTo(100) / 100f;
+            lastdamage = (int)(newDamage * rangedOffset);
+        }
+
+        /// <summary>
+        /// 监测与攻击
+        /// </summary>
+        void Attack(NPC target1) {
+            //发射
+            if (count >= Gcount) {
+                //普通
+                if (Main.rand.Next(100) >= Owner.GetCritChance<GenericDamageClass>() + (int)Projectile.ai[1]) {
+                    for (int i = 0; i < 1; i++) {
+                        Terraria.Audio.SoundEngine.PlaySound(SoundID.NPCHit9);
+                        Projectile proj1 = Projectile.NewProjectileDirect(new EntitySource_Parent(Projectile), Projectile.Center + new Vector2(0, 20), new Vector2(4, 0), ModContent.ProjectileType<Corrupt2Proj0>(), lastdamage, 1, Owner.whoAmI);
+                        Projectile proj3 = Projectile.NewProjectileDirect(new EntitySource_Parent(Projectile), Projectile.Center + new Vector2(0, 20), new Vector2(-4, 0), ModContent.ProjectileType<Corrupt2Proj0>(), lastdamage, 1, Owner.whoAmI);
+                        if (Projectile.ModProjectile is GOGDT proj0 && proj0.OrichalcumMarkDT) {
+                            if (proj1.ModProjectile is GOGProj proj2) {
+                                proj2.OrichalcumMarkProj = true;
+                                proj2.OrichalcumMarkProjcount = 300;
+                            }
+
+                            if (proj3.ModProjectile is GOGProj proj4) {
+                                proj4.OrichalcumMarkProj = true;
+                                proj4.OrichalcumMarkProjcount = 300;
+                            }
+                        }
+                    }
+                }
+
+                //过载
+                if (Main.rand.Next(100) < Owner.GetCritChance<GenericDamageClass>() + (int)Projectile.ai[1]) {
+                    for (int i = 0; i < 1; i++) {
+                        Terraria.Audio.SoundEngine.PlaySound(SoundID.NPCHit9);
+                        Projectile proj1 = Projectile.NewProjectileDirect(new EntitySource_Parent(Projectile), Projectile.Center + new Vector2(0, 20), new Vector2(4, 0), ModContent.ProjectileType<Corrupt2Proj0>(), lastdamage, 1, Owner.whoAmI, 1);
+                        Projectile proj3 = Projectile.NewProjectileDirect(new EntitySource_Parent(Projectile), Projectile.Center + new Vector2(0, 20), new Vector2(-4, 0), ModContent.ProjectileType<Corrupt2Proj0>(), lastdamage, 1, Owner.whoAmI, 1);
+                        if (Projectile.ModProjectile is GOGDT proj0 && proj0.OrichalcumMarkDT) {
+                            if (proj1.ModProjectile is GOGProj proj2) {
+                                proj2.OrichalcumMarkProj = true;
+                                proj2.OrichalcumMarkProjcount = 300;
+                            }
+
+                            if (proj3.ModProjectile is GOGProj proj4) {
+                                proj4.OrichalcumMarkProj = true;
+                                proj4.OrichalcumMarkProjcount = 300;
+                            }
+                        }
+                    }
+                }
+
+                //计时重置
+                count = Owner.GetModPlayer<GOGModPlayer>().GcountEx;
+            }
+        }
+
+        /// <summary>
+        /// 感染攻击
+        /// </summary>
+        /// <param name="target1"></param>
+        void Attack2(NPC target1) {
+
+        }
+
+        public override Color? GetAlpha(Color lightColor) {
+            return Color.White;
+        }
+
+        public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac) {
+            fallThrough = false;
+            return base.TileCollideStyle(ref width, ref height, ref fallThrough, ref hitboxCenterFrac);
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity) {
+            //Projectile.velocity *= 0;
+            return false;
+        }
+
+
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox) {
+            return false;
+        }
+
+        public override void OnKill(int timeLeft) {
+            //爆炸粒子
+            for (int j = 0; j < 15; j++) {
+                int num1 = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Wraith, 0f, 0f, 10, Color.White, 0.8f);
+                Main.dust[num1].noGravity = true;
+                Main.dust[num1].velocity *= 2f;
+            }
+            for (int j = 0; j < 15; j++) {
+                int num2 = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Wraith, 0f, 0f, 10, Color.White, 0.4f);
+                Main.dust[num2].noGravity = true;
+                Main.dust[num2].velocity *= 1f;
+            }
+        }
+
+        public override bool PreDraw(ref Color lightColor) {
+            Texture2D texture0 = ModContent.Request<Texture2D>(GOGConstant.Projectiles + "Corrupt2DT").Value;
+            Vector2 drawPosition0 = Projectile.Center - Main.screenPosition + new Vector2(0, -14);
+            Main.EntitySpriteDraw(texture0, drawPosition0, null, lightColor, Projectile.rotation, texture0.Size() * 0.5f, Projectile.scale, SpriteEffects.None, 0);
+
+            return false;
+        }
+    }
+}
---
+++ Content/Projectiles/Corrupt2Proj1.cs
@@ -0,0 +1,135 @@
+﻿using GloryofGuardian.Common;
+using Terraria.DataStructures;
+using Terraria.ID;
+
+namespace GloryofGuardian.Content.Projectiles
+{
+    public class Corrupt2Proj1 : GOGProj
+    {
+        public override string Texture => GOGConstant.Projectiles + Name;
+
+        public override void SetStaticDefaults() {
+            //残影机制
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 8;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+        }
+
+        public override void SetDefaults() {
+            //这里的尺寸对应的是碰撞体积
+            Projectile.width = 16;
+            Projectile.height = 16;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.DamageType = GuardianDamageClass.Instance;
+            Projectile.timeLeft = 600;
+            Projectile.ignoreWater = true;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 20;
+            Projectile.aiStyle = -1;
+            Projectile.penetrate = -1;//穿透数，1为攻击到第一个敌人就消失
+            Projectile.tileCollide = true;
+
+            Projectile.light = 1f;
+
+            Projectile.scale *= 1f;
+        }
+
+        Player Owner => Main.player[Projectile.owner];
+
+        Vector2 OwnerPos => Owner.Center;
+
+        Vector2 ToMou => Main.MouseWorld - OwnerPos;
+
+        int projtype = 0;
+        Vector2 spawnpos = new Vector2(0, 0);
+        public override void OnSpawn(IEntitySource source) {
+        }
+
+        int count = 0;
+        int height0 = 0;
+        public override void AI() {
+            count++;
+
+            height0 = (int)(180 - 20 * Projectile.ai[1]);
+            Projectile.velocity *= 0;
+
+            float dying = count / 20f;
+
+            if (Projectile.ai[0] == 0) {
+                if (count % 1 == 0) {
+                    for (int i = 0; i < 4; i++) {
+                        int num = Dust.NewDust(Projectile.position + new Vector2(0, Projectile.height - height0 * dying - 16), 16, 32, DustID.CorruptGibs, 0f, 0f, 50, Color.White, 0.5f);
+                        Main.dust[num].velocity = new Vector2(0, -8);
+                        Main.dust[num].noGravity = true;
+                    }
+                }
+
+                if (count % 1 == 0) {
+                    for (int i = 0; i < 1; i++) {
+                        int num = Dust.NewDust(Projectile.position + new Vector2(0, Projectile.height - height0 * dying - 16), 16, 32, DustID.CorruptGibs, 0f, 0f, 50, Color.White, 1.5f);
+                        Main.dust[num].velocity = new Vector2(0, -2);
+                        Main.dust[num].noGravity = true;
+                    }
+                }
+            }
+
+            if (Projectile.ai[0] == 1) {
+                if (count % 1 == 0) {
+                    for (int i = 0; i < 4; i++) {
+                        int num = Dust.NewDust(Projectile.position + new Vector2(0, Projectile.height - height0 * dying - 16), 16, 32, DustID.GreenFairy, 0f, 0f, 50, Color.Yellow, 1f);
+                        Main.dust[num].velocity = new Vector2(0, -4);
+                        Main.dust[num].noGravity = true;
+                    }
+                }
+
+                if (count % 1 == 0) {
+                    for (int i = 0; i < 1; i++) {
+                        int num = Dust.NewDust(Projectile.position + new Vector2(0, Projectile.height - height0 * dying - 16), 16, 32, DustID.CursedTorch, 0f, 0f, 50, Color.White, 2f);
+                        Main.dust[num].velocity = new Vector2(0, -2);
+                        Main.dust[num].noGravity = true;
+                    }
+                }
+            }
+
+            if (count > 20) Projectile.Kill();
+        }
+
+        public override Color? GetAlpha(Color lightColor) {
+            return base.GetAlpha(lightColor);
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
+            if (target.knockBackResist > 0) target.velocity *= 0;
+            if (target.knockBackResist > 0) target.velocity += new Vector2(0, -height0 / 40f);
+            if (target.knockBackResist > 0) target.velocity += new Vector2(Projectile.Center.Toz(target.Center).X * 24f, 0);
+
+            if (Projectile.ai[0] == 1) target.AddBuff(BuffID.CursedInferno, 180);
+        }
+
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox) {
+            Rectangle mergedCollisionArea = new Rectangle(
+                (int)Projectile.position.X, // 左上角X坐标
+                (int)Projectile.position.Y + Projectile.height - height0, // 左上角Y坐标 + 弹幕高度 - 80像素
+                Projectile.width, // 宽度
+                Projectile.height + 80 // 高度（弹幕高度 + 80像素）
+            );
+
+            // 检测目标是否在合并后的碰撞区域内
+            if (targetHitbox.Intersects(mergedCollisionArea)) {
+                return true;
+            }
+
+            // 如果不在碰撞区域内，返回false
+            return false;
+
+            //return base.Colliding(projHitbox, targetHitbox);
+        }
+
+        public override void OnKill(int timeLeft) {
+        }
+
+        public override bool PreDraw(ref Color lightColor) {
+            return false;
+        }
+    }
+}
